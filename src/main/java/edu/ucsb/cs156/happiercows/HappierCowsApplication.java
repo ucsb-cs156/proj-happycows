321b8711b5a93aa1f75933b61e73b97c478d3b3f
shuningliu <shuningliu@ucsb.edu>
2022-11-17 14:23:37 -0800

    now it shows pacific time as required

--- src/main/java/edu/ucsb/cs156/happiercows/HappierCowsApplication.java
+++ src/main/java/edu/ucsb/cs156/happiercows/HappierCowsApplication.java
@@ -1,18 +1,31 @@
 package edu.ucsb.cs156.happiercows;
 
+import java.time.ZonedDateTime;
+import java.util.Optional;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.auditing.DateTimeProvider;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
-@EnableJpaAuditing
+@EnableJpaAuditing(dateTimeProviderRef = "utcDateTimeProvider")
 @EnableAsync
 public class HappierCowsApplication {
-
+  @Bean
+    public DateTimeProvider utcDateTimeProvider() {
+        return () -> {
+          ZonedDateTime now = ZonedDateTime.now();
+          return Optional.of(now);
+        };
+    }
   public static void main(String[] args) {
     SpringApplication.run(HappierCowsApplication.class, args);
+    
+
   }
+
 
 }
