0781bccb0b166fd56f2ce0fa60c192d662b4a8cc
Sergio Colis <sergioacolis@Sergios-MacBook-Pro.local>
2022-11-28 13:52:11 -0800

    SC - Added carryingCapacity to commons, createcommonsparams, and commonscontroller

--- src/main/java/edu/ucsb/cs156/happiercows/entities/Commons.java
+++ src/main/java/edu/ucsb/cs156/happiercows/entities/Commons.java
@@ -32,6 +32,7 @@ public class Commons
   private LocalDateTime endingDate;
   private double degradationRate;
   private boolean showLeaderboard;
+  private Integer carryingCapacity;
 
   @ManyToMany(fetch = FetchType.EAGER, cascade = {CascadeType.PERSIST,CascadeType.REMOVE})
   @JoinTable(name = "user_commons",
