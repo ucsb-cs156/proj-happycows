b0671a2ec8b0a5336c6aaff6f1ad5fe4ec405d84
irenecho75 <irenecho75@gmail.com>
2022-11-28 11:55:43 -0800

    ic - fix assertion count

--- src/main/java/edu/ucsb/cs156/happiercows/repositories/CowDeathRepository 2.java
+++
@@ -1,8 +0,0 @@
-package edu.ucsb.cs156.happiercows.repositories;
-import edu.ucsb.cs156.happiercows.entities.CowDeath;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-public interface CowDeathRepository extends CrudRepository<CowDeath, Long> {
-    
-}
