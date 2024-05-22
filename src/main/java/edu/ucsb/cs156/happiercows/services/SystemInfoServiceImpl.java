3b59a540456b8368a098b7670a52ceca6f7d2927
chendashi <chendashi111@outlook.com>
2024-05-22 06:57:17 +0000

    zc - commit tracker, change to new branch for cleaner commit

--- src/main/java/edu/ucsb/cs156/happiercows/services/SystemInfoServiceImpl.java
+++ src/main/java/edu/ucsb/cs156/happiercows/services/SystemInfoServiceImpl.java
@@ -22,21 +22,42 @@ public class SystemInfoServiceImpl extends SystemInfoService {
   @Value("${app.showSwaggerUILink:false}")
   private boolean showSwaggerUILink;
 
+  @Value("${app.startQtrYYYYQ:20242}")
+  private String startQtrYYYYQ;
+
+  @Value("${app.endQtrYYYYQ:20243}")
+  private String endQtrYYYYQ;
+
   @Value("${app.sourceRepo}")
-  private String sourceRepo = "https://github.com/ucsb-cs156/proj-happycows";
+  private String sourceRepo = "https://github.com/ucsb-cs156-s24/proj-happycows-s24-4pm-7";
+
+  @Value("${git.commit.message.short:unknown}")
+  private String commitMessage;
+
+  @Value("${git.commit.id.abbrev:unknown}")
+  private String commitId;
 
   @Value("${app.oauth.login:/oauth2/authorization/google}")
   private String oauthLogin;
 
-  public SystemInfo getSystemInfo() {
-    SystemInfo si = SystemInfo.builder()
-    .springH2ConsoleEnabled(this.springH2ConsoleEnabled)
-    .showSwaggerUILink(this.showSwaggerUILink)
-    .sourceRepo(this.sourceRepo)
-    .oauthLogin(this.oauthLogin)
-    .build();
-  log.info("getSystemInfo returns {}",si);
-  return si;
+  public static String githubUrl(String repo, String commit) {
+    return commit != null && repo != null ? repo + "/commit/" + commit : null;
   }
 
+  public SystemInfo getSystemInfo() {
+    SystemInfo si =
+        SystemInfo.builder()
+            .springH2ConsoleEnabled(this.springH2ConsoleEnabled)
+            .showSwaggerUILink(this.showSwaggerUILink)
+            .startQtrYYYYQ(this.startQtrYYYYQ)
+            .endQtrYYYYQ(this.endQtrYYYYQ)
+            .sourceRepo(this.sourceRepo)
+            .commitMessage(this.commitMessage)
+            .commitId(this.commitId)
+            .githubUrl(githubUrl(this.sourceRepo, this.commitId))
+            .oauthLogin(this.oauthLogin)
+            .build();
+    log.info("getSystemInfo returns {}", si);
+    return si;
+  }
 }
