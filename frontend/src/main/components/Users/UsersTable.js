4f3074bdcf474e7a8ac51d0ed8e3718597059133
jeffmun <61306390+Jumshim@users.noreply.github.com>
2024-05-27 20:19:49 -0700

    jm - added buttons and modals for suspend and restore

--- frontend/src/main/components/Users/UsersTable.js
+++ frontend/src/main/components/Users/UsersTable.js
@@ -1,39 +1,113 @@
-import React from "react";
-import OurTable from "main/components/OurTable"
+import React, { useState } from "react";
+import OurTable, { ButtonColumn } from "main/components/OurTable";
 import { formatTime } from "main/utils/dateUtils";
-
-const columns = [
-    {
-        Header: 'id',
-        accessor: 'id', // accessor is the "key" in the data
-    },
-    {
-        Header: 'First Name',
-        accessor: 'givenName',
-    },
-    {
-        Header: 'Last Name',
-        accessor: 'familyName',
-    },
-    {
-        Header: 'Email',
-        accessor: 'email',
-    },
-    {
-        Header: 'Last Online',
-        id: 'lastOnline',
-        accessor: (row) => formatTime(row.lastOnline),
-    },
-    {
-        Header: 'Admin',
-        id: 'admin',
-        accessor: (row, _rowIndex) => String(row.admin) // hack needed for boolean values to show up
-    },
-];
+import { restoreUser, suspendUser } from "main/utils/users";
+import { Button, Modal } from "react-bootstrap";
 
 export default function UsersTable({ users }) {
-    return <OurTable
-        data={users}
-        columns={columns}
-        testid={"UsersTable"} />;
-};
+  const [showSuspendModal, setShowSuspendModal] = useState(false);
+  const [showRestoreModal, setShowRestoreModal] = useState(false);
+  const [selectedCell, setSelectedCell] = useState(null);
+
+  const suspendCallback = async (cell) => {
+    setSelectedCell(cell);
+    setShowSuspendModal(true);
+  };
+
+  const restoreCallback = async (cell) => {
+    setSelectedCell(cell);
+    setShowRestoreModal(true);
+  };
+
+  const confirmSuspend = async () => {
+    suspendUser(selectedCell);
+    setShowSuspendModal(false);
+  };
+
+  const confirmRestore = async () => {
+    restoreUser(selectedCell);
+    setShowRestoreModal(false);
+  };
+
+  const columns = [
+    {
+      Header: "id",
+      accessor: "id", // accessor is the "key" in the data
+    },
+    {
+      Header: "First Name",
+      accessor: "givenName",
+    },
+    {
+      Header: "Last Name",
+      accessor: "familyName",
+    },
+    {
+      Header: "Email",
+      accessor: "email",
+    },
+    {
+      Header: "Last Online",
+      id: "lastOnline",
+      accessor: (row) => formatTime(row.lastOnline),
+    },
+    {
+      Header: "Admin",
+      id: "admin",
+      accessor: (row, _rowIndex) => String(row.admin), // hack needed for boolean values to show up
+    },
+    {
+      Header: "Suspended",
+      id: "suspended",
+      accessor: (row, _rowIndex) => String(row.suspended), // hack needed for boolean values to show up
+    },
+    ButtonColumn("Suspend", "danger", suspendCallback, "UsersTable"),
+    ButtonColumn("Restore", "primary", restoreCallback, "UsersTable"),
+  ];
+
+  const suspendModal = (
+    <Modal show={showSuspendModal} onHide={() => setShowSuspendModal(false)}>
+      <Modal.Header closeButton>
+        <Modal.Title>Suspend User</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        Are you sure you want to suspend {selectedCell?.row.values.email}?
+      </Modal.Body>
+      <Modal.Footer>
+        <Button variant="secondary" onClick={() => setShowSuspendModal(false)}>
+          Cancel
+        </Button>
+        <Button variant="danger" onClick={confirmSuspend}>
+          Suspend
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  );
+
+  const restoreModal = (
+    <Modal show={showRestoreModal} onHide={() => setShowRestoreModal(false)}>
+      <Modal.Header closeButton>
+        <Modal.Title>Restore User</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        Are you sure you want to restore {selectedCell?.row.values.email}?
+      </Modal.Body>
+      <Modal.Footer>
+        <Button variant="secondary" onClick={() => setShowRestoreModal(false)}>
+          Cancel
+        </Button>
+        <Button variant="primary" onClick={confirmRestore}>
+          Restore
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  );
+
+  return (
+    <>
+      <OurTable data={users} columns={columns} testid={"UsersTable"} />
+      {suspendModal}
+      {restoreModal}
+    </>
+  );
+}
--- frontend/src/main/utils/users.js
+++ frontend/src/main/utils/users.js
@@ -2,16 +2,56 @@ import { useQuery } from "react-query";
 import axios from "axios";
 
 export function useUsers() {
-  return useQuery("users", async () => {
-    const uri = "/api/admin/users";
-    try {
-      const response = await axios.get(uri);      
-      return response.data ;
-    } catch (e) {
-      console.error(`Error getting data from ${uri}:`,e);
-      return [];
+  return useQuery(
+    "users",
+    async () => {
+      const uri = "/api/admin/users";
+      try {
+        const response = await axios.get(uri);
+        return response.data;
+      } catch (e) {
+        console.error(`Error getting data from ${uri}:`, e);
+        return [];
+      }
+    },
+    {
+      initialData: [],
     }
-  }, {
-    initialData: []
-  });
+  );
+}
+
+export function suspendUser(cell) {
+  const uri = "/api/admin/users/suspend";
+  const user = cell.row.values;
+  try {
+    const response = axios.post(
+      uri,
+      {},
+      {
+        params: {
+          userId: user.id,
+        },
+      }
+    );
+    return response.data;
+  } catch (e) {
+    console.error(`Error suspending user with id ${user.id} from ${uri}`, e);
+    return [];
+  }
+}
+
+export function restoreUser(cell) {
+  const uri = "/api/admin/users/restore";
+  const user = cell.row.values;
+  try {
+    const response = axios.post(cell.row.values, [], {
+      params: {
+        userId: user.id,
+      },
+    });
+    return response.data;
+  } catch (e) {
+    console.error(`Error restoring user with id ${user.id} from ${uri}`, e);
+    return [];
+  }
 }
