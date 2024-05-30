745d09d5e6e644594f871247dce9555c1d5f36c7
chendashi <chendashi111@outlook.com>
2024-05-30 11:07:32 +0000

    Revert "zc - list announcement page created for admin user"
    This reverts commit 274b1eca48599b0754a944994607a5463312847b.

--- frontend/src/main/pages/AdminListAnnouncementsPage.js
+++ frontend/src/main/pages/AdminListAnnouncementsPage.js
@@ -1,41 +1,15 @@
-import React from "react";
+import React from 'react';
 import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
-import AnnouncementTable from 'main/components/Announcement/AnnouncementTable';
-import { useBackend } from 'main/utils/useBackend';
-import { useCurrentUser } from "main/utils/currentUser";
-import { Button } from "react-bootstrap";
-import { useParams } from "react-router-dom";
 
-export default function AdminListAnnouncementsPage()
-{
-    const { commonsId } = useParams();
-    const { data: currentUser} = useCurrentUser();
-
-    // Stryker disable all
-    const { data: announcements, error: _error, status: _status } = useBackend(
-        [`/api/announcements/all?commonsId=${commonsId}`],
-        { method: "GET", url: `/api/announcements/all`, params: { commonsId } },
-        []
-    );
-    // Stryker restore all
-
-    const createButton = () => {
-        return (
-            <Button href={`/admin/announcements/${commonsId}/create`}
-            style={{ float: "right" }}
-            >
-                Create Announcement
-            </Button>
-        );
-    }
-
-    return (
-        <BasicLayout>
-            <div className="pt-2">
-                <h2>Announcements for Commons {commonsId}</h2>
-                {createButton()}
-                <AnnouncementTable announcements={announcements} currentUser={currentUser} />
-            </div>
-        </BasicLayout>
-  );
+const AdminListAnnouncementsPage = () => {
+    // Stryker disable all : placeholder for future implementation
+  return (
+    <BasicLayout>
+      <div className="pt-2">
+        <h1>Admin List Announcements page not yet implemented</h1>
+      </div>
+    </BasicLayout>
+  )
 };
+
+export default AdminListAnnouncementsPage;
--- frontend/src/tests/pages/AdminListAnnouncementsPage.test.js
+++ frontend/src/tests/pages/AdminListAnnouncementsPage.test.js
@@ -1,31 +1,19 @@
-import { render, screen, waitFor } from "@testing-library/react";
+import React from 'react';
+import { render, screen } from "@testing-library/react";
+import AdminListAnnouncementsPage from 'main/pages/AdminListAnnouncementsPage';
 import { QueryClient, QueryClientProvider } from "react-query";
-import { MemoryRouter, useParams } from "react-router-dom";
-import axios from "axios";
-import AxiosMockAdapter from "axios-mock-adapter";
-import AdminListAnnouncementsPage from "main/pages/AdminListAnnouncementsPage";
+import { MemoryRouter } from "react-router-dom";
+
 import { apiCurrentUserFixtures } from "fixtures/currentUserFixtures";
 import { systemInfoFixtures } from "fixtures/systemInfoFixtures";
-import { announcementFixtures } from "fixtures/announcementFixtures";
+import axios from "axios";
+import AxiosMockAdapter from "axios-mock-adapter";
 
-const mockToast = jest.fn();
-jest.mock('react-toastify', () => {
-    const originalModule = jest.requireActual('react-toastify');
-    return {
-        __esModule: true,
-        ...originalModule,
-        toast: (x) => mockToast(x)
-    };
-});
-
-jest.mock('react-router-dom', () => ({
-    ...jest.requireActual('react-router-dom'),
-    useParams: jest.fn(),
-}));
 
 describe("AdminListAnnouncementsPage tests", () => {
+
     const axiosMock = new AxiosMockAdapter(axios);
-    const testId = "AnnouncementTable";
+
 
     const setupAdminUser = () => {
         axiosMock.reset();
@@ -34,15 +22,13 @@ describe("AdminListAnnouncementsPage tests", () => {
         axiosMock.onGet("/api/systemInfo").reply(200, systemInfoFixtures.showingNeither);
     };
 
-    test("Renders Create Button for admin user", async () => {
+    const queryClient = new QueryClient();
+    test("Renders expected content", () => {
+        // arrange
+
         setupAdminUser();
-        const queryClient = new QueryClient();
-        const commonsId = 1;
 
-        useParams.mockReturnValue({ commonsId });
-
-        axiosMock.onGet(`/api/announcements/all`, { params: { commonsId } }).reply(200, []);
-
+        // act
         render(
             <QueryClientProvider client={queryClient}>
                 <MemoryRouter>
@@ -51,34 +37,8 @@ describe("AdminListAnnouncementsPage tests", () => {
             </QueryClientProvider>
         );
 
-        await waitFor(() => {
-            expect(screen.getByText(/Create Announcement/)).toBeInTheDocument();
-        });
-        const button = screen.getByText(/Create Announcement/);
-        expect(button).toHaveAttribute("style", "float: right;");
-        expect(button).toHaveAttribute("href", "/admin/announcements/1/create");
+        // assert
+        expect(screen.getByText("Admin List Announcements page not yet implemented")).toBeInTheDocument();
     });
 
-    test("Announcements listed for admin user", async () => {
-        setupAdminUser();
-        const queryClient = new QueryClient();
-        const commonsId = 1;
-
-        useParams.mockReturnValue({ commonsId });
-
-        axiosMock.onGet(`/api/announcements/all`, { params: { commonsId } }).reply(200, announcementFixtures.threeAnnouncements);
-
-        render(
-            <QueryClientProvider client={queryClient}>
-                <MemoryRouter>
-                    <AdminListAnnouncementsPage />
-                </MemoryRouter>
-            </QueryClientProvider>
-        );
-        await waitFor(() => {
-            expect(screen.getByTestId(`${testId}-cell-row-0-col-id`)).toHaveTextContent("1");
-        });
-        expect(screen.getByTestId(`${testId}-cell-row-1-col-id`)).toHaveTextContent("2");
-        expect(screen.getByTestId(`${testId}-cell-row-2-col-id`)).toHaveTextContent("3");
-    });
 });
