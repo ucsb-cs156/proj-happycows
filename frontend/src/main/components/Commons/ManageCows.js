5d285c368e80bbbdba0b009f4f254440866284ab
Nathaniel Mitter <130000100+SexyJesusFreak@users.noreply.github.com>
2024-11-20 15:59:02 -0800

    NM - formatted buy and sell neater

--- frontend/src/main/components/Commons/ManageCows.js
+++ frontend/src/main/components/Commons/ManageCows.js
@@ -65,9 +65,16 @@ const ManageCows = ({ userCommons, commons, setMessage, openModal }) => {
                             </Col>
                         </Row>
                         <p>
-                            Note: Buying cows buys at the current cow price, but
-                            selling cows sells at the current cow price times
-                            the average health of cows as a percentage!
+                        <div>
+                            <h3>Note:</h3>
+                            <ul>
+                                <li>Buying cows buys at the current cow price.</li>
+                                <li>
+                                    Selling cows sells at the current cow price multiplied by
+                                    the average cow health (as a percentage).
+                                </li>
+                            </ul>
+                        </div>
                         </p>
                     </>
                 )}
