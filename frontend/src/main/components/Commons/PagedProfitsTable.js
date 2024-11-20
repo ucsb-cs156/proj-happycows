307cc790ba90a9557b6f1a1d5371126d33f15823
jzipkin1812 <javinzipkin@gmail.com>
2024-11-19 18:40:11 -0800

    added a test

--- frontend/src/main/components/Commons/PagedProfitsTable.js
+++ frontend/src/main/components/Commons/PagedProfitsTable.js
@@ -90,8 +90,10 @@ const PagedProfitsTable = () => {
             <p>Page: {selectedPage + 1}</p>
             <Button data-testid={`${testId}-previous-button`}onClick={previousPageCallback()} disabled={ selectedPage === 0}>Previous</Button>
             <Button data-testid={`${testId}-next-button`} onClick={nextPageCallback()} disabled={page.totalPages===0 || selectedPage === page.totalPages-1}>Next</Button>
-            {/* Stryker disable next-line all : don't test CSS params*/}
-            <div style={{display: 'flex'}}>
+            <div 
+                data-testid="PagedProfitsTable-container" 
+                style={{display: 'flex'}}
+            >
                     < OurTable
                         data={page.content}
                         columns={columns}
