2f378f093bac5d7b1f1cecbc83ce4a13b7c67c7a
Aden <adenjo@ucsb.edu>
2024-11-20 18:18:49 -0800

    Adding Course Table to check on Dokku

---
+++ src/main/java/edu/ucsb/cs156/happiercows/entities/Course.java
@@ -0,0 +1,26 @@
+package edu.ucsb.cs156.happiercows.entities;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder
+@Entity(name = "courses")
+public class Course {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private long id;
+
+  private String name;
+  // private String school;
+  private String term;
+  private LocalDateTime startDate;
+  private LocalDateTime endDate;
+  // private String githubOrg;
+
+}
