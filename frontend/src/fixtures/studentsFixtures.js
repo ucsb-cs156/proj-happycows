69a4aacb5c418ae3bb211c29322441b09aec5d0c
Alec Song <alecsong@Alecs-MacBook-Pro.local>
2024-11-21 22:32:15 -0800

    Fixed npm issues

---
+++ frontend/src/fixtures/studentsFixtures.js
@@ -0,0 +1,37 @@
+const studentsFixtures = {
+    oneStudent: {
+            "id": 1,
+            "lName": "test1",
+            "fmName": "test1m",
+            "perm": "1234567",
+            "courseId": "123",
+            "email": "test1@hotmail.com"
+    },
+    threeStudents: [
+        {
+            "id": 1,
+            "lName": "test1",
+            "fmName": "test1m",
+            "perm": "1234567",
+            "courseId": "123",
+            "email": "test1@hotmail.com"
+        },
+        {
+            "id": 2,
+            "lName": "test2",
+            "fmName": "test2m",
+            "perm": "1234568",
+            "courseId": "124",
+            "email": "test2@hotmail.com"
+        },
+        {
+            "id": 3,
+            "lName": "test3",
+            "fmName": "test3m",
+            "perm": "1234569",
+            "courseId": "125",
+            "email": "test3@hotmail.com"
+        },
+    ]
+};
+export { studentsFixtures };
