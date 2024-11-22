7bea0a370bc0208475b7b8e67c31509f4e57f8ef
Justin Wu <justin_wu@ucsb.edu>
2024-11-22 15:02:39 -0800

    remove unnecessary stryker pragmas

--- frontend/src/main/pages/AdminStudentsIndexPage.js
+++ frontend/src/main/pages/AdminStudentsIndexPage.js
@@ -1,7 +1,6 @@
 import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
 
 export default function AdminStudentsIndexPage() {
-  // Stryker disable all : placeholder for future implementation
   return (
     <BasicLayout>
       <div className="pt-2">
