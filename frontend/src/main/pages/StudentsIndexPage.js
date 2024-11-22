1d279a391f1ebcac97bcdd915fae40405692f595
Alec Song <alecsong@Alecs-MacBook-Pro.local>
2024-11-22 00:30:06 -0800

    added Index page; may need to rewrite tests for users

--- frontend/src/main/pages/StudentsIndexPage.js
+++ frontend/src/main/pages/StudentsIndexPage.js
@@ -1,17 +1,36 @@
+import React from "react";
+import { useBackend } from "main/utils/useBackend";
+
 import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
+import StudentsTable from "main/components/Students/StudentsTable";
+import { Button } from "react-bootstrap";
+import { useCurrentUser} from "main/utils/currentUser";
 
 export default function StudentsIndexPage() {
-  // Stryker disable all : placeholder for future implementation
+  const currentUser = useCurrentUser();
+
+  const {
+    data: students,
+    error: _error,
+    status: _status,
+  } = useBackend(
+    // Stryker disable next-line all : don't test internal caching of React Query
+    ["/api/Students/all"],
+    { method: "GET", url: "/api/Students/all" },
+    [],
+  );
+
   return (
     <BasicLayout>
       <div className="pt-2">
-        <h1>Index page not yet implemented</h1>
-        <p>
-          <a href="/admin/Students/create">Create</a>
-        </p>
-        <p>
-          <a href="/admin/Students/edit/1">Edit</a>
-        </p>
+        <h1>Students</h1>
+        <StudentsTable
+          students={students}
+          currentUser={currentUser}
+        />
+        <Button variant = "primary" href = {`/admin/Students/create`} >
+            Create Student
+        </Button>
       </div>
     </BasicLayout>
   );
