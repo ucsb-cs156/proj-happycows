8649ef4113af08a0e8df72a14cea31c211c8c01f
Aden <adenjo@ucsb.edu>
2024-11-23 22:06:23 -0800

    GET and POST added

--- src/main/java/edu/ucsb/cs156/happiercows/entities/Courses.java
+++ src/main/java/edu/ucsb/cs156/happiercows/entities/Courses.java
@@ -4,23 +4,16 @@ import lombok.*;
 
 import javax.persistence.*;
 
-import java.time.LocalDateTime;
-
 @Data
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Builder
 @Entity(name = "courses")
-public class Course {
+public class Courses {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private long id;
 
   private String name;
-  // private String school;
   private String term;
-  private LocalDateTime startDate;
-  private LocalDateTime endDate;
-  // private String githubOrg;
-
 }
