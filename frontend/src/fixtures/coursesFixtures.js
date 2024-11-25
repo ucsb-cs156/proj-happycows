33df0eac512a9ccf3df9f40f69d56d8b45ea7ee0
Aden <adenjo@ucsb.edu>
2024-11-25 01:55:03 -0800

    Finished fixtures for courses

---
+++ frontend/src/fixtures/coursesFixtures.js
@@ -0,0 +1,27 @@
+const coursesFixtures = {
+    oneCourse: {
+        "id": 1,
+        "name": "CS156",
+        "term": "F23",
+    },
+    threeCourses: [
+        { 
+            "id": 1,
+            "name": "CS156",
+            "term": "F23",
+        },
+        {
+            "id": 2,
+            "name": "CS185",
+            "term": "W24",
+        },
+        {
+            "id": 3,
+            "name": "CS170",
+            "term": "S24",
+        }
+    ]
+};
+
+
+export { coursesFixtures };
