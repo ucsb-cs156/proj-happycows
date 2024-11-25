a240024c94d72cfee8f7074bea9fa065ababc8e3
Aden <adenjo@ucsb.edu>
2024-11-25 13:08:01 -0800

    Storybook not running

---
+++ frontend/src/main/components/Courses/CoursesTable.js
@@ -0,0 +1,62 @@
+import React from "react";
+ import OurTable, { ButtonColumn } from "main/components/OurTable"
+ import { useBackendMutation } from "main/utils/useBackend";
+ import { cellToAxiosParamsDelete, onDeleteSuccess } from "main/components/Utils/CoursesUtils"
+ import { useNavigate } from "react-router-dom";
+ import { hasRole } from "main/utils/currentUser";
+
+ export default function CoursesTable({ courses, currentUser }) {
+
+     const navigate = useNavigate();
+
+     const joinCallback = (cell) => {
+        navigate(`/courses/join/${cell.row.values.id}`);
+    };
+
+     const editCallback = (cell) => {
+         navigate(`/courses/edit/${cell.row.values.id}`);
+     };
+
+     // Stryker disable all : hard to test for query caching
+
+     const deleteMutation = useBackendMutation(
+         cellToAxiosParamsDelete,
+         { onSuccess: onDeleteSuccess },
+         ["/api/courses/all"]
+     );
+     // Stryker restore all 
+
+     // Stryker disable next-line all : TODO try to make a good test for this
+     const deleteCallback = async (cell) => { deleteMutation.mutate(cell); }
+
+     const columns = [
+         {
+             Header: 'id',
+             accessor: 'id',
+         },
+         {
+             Header: 'Name',
+             accessor: 'name',
+         },
+         {
+             Header: 'Term',
+             accessor: 'term',
+         },
+     ];
+
+     if (hasRole(currentUser, "ROLE_ADMIN")) {
+         columns.push(ButtonColumn("Edit", "primary", editCallback, "CoursesTable"));
+         columns.push(ButtonColumn("Delete", "danger", deleteCallback, "CoursesTable"));
+     }
+     
+     columns.push(ButtonColumn("Join", "primary", joinCallback, "CoursesTable"));
+
+
+
+     return (
+        <>
+            <div>Total Courses: {courses.length}</div>
+          <OurTable data={courses} columns={columns} testid={"CoursesTable"} />
+        </>
+      );
+    };
---
+++ frontend/src/tests/components/Courses/CoursesTable.test.js
@@ -0,0 +1,268 @@
+import CoursesTable from "main/components/Courses/CoursesTable"
+import { fireEvent, render, waitFor, screen } from "@testing-library/react";
+import { coursesFixtures } from "fixtures/coursesFixtures";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { MemoryRouter } from "react-router-dom";
+import { currentUserFixtures } from "fixtures/currentUserFixtures";
+
+
+const mockedNavigate = jest.fn();
+
+jest.mock('react-router-dom', () => ({
+    ...jest.requireActual('react-router-dom'),
+    useNavigate: () => mockedNavigate
+}));
+
+describe("UserTable tests", () => {
+  const queryClient = new QueryClient();
+
+  test("Has the expected column headers and content for ordinary user", () => {
+
+    const currentUser = currentUserFixtures.userOnly;
+
+    render(
+      <QueryClientProvider client={queryClient}>
+        <MemoryRouter>
+          <CoursesTable courses={coursesFixtures.threeCourses} currentUser={currentUser} />
+        </MemoryRouter>
+      </QueryClientProvider>
+
+    );
+
+    const expectedHeaders = ["id", "Name", "Term"];
+    const expectedFields = ["id", "name", "term"];
+    const testId = "CoursesTable";
+
+    expectedHeaders.forEach((headerText) => {
+      const header = screen.getByText(headerText);
+      expect(header).toBeInTheDocument();
+    });
+
+    expectedFields.forEach((field) => {
+      const header = screen.getByTestId(`${testId}-cell-row-0-col-${field}`);
+      expect(header).toBeInTheDocument();
+    });
+
+    expect(screen.getByTestId(`${testId}-cell-row-0-col-id`)).toHaveTextContent("1");
+    expect(screen.getByTestId(`${testId}-cell-row-1-col-id`)).toHaveTextContent("2");
+
+    const joinButton = screen.queryByTestId(`${testId}-cell-row-0-col-Join-button`);
+    expect(joinButton).toBeInTheDocument();
+    expect(joinButton).toHaveClass("btn-primary");
+
+
+    const editButton = screen.queryByTestId(`${testId}-cell-row-0-col-Edit-button`);
+    expect(editButton).not.toBeInTheDocument();
+
+    const deleteButton = screen.queryByTestId(`${testId}-cell-row-0-col-Delete-button`);
+    expect(deleteButton).not.toBeInTheDocument();
+
+    const totalCoursesElement = screen.getByText("Total Courses: 3"); // Assuming there are 3 courses in the fixture
+    expect(totalCoursesElement).toBeInTheDocument();
+
+  });
+
+
+  
+  test("renders empty table correctly", () => {
+
+    // arrange
+    const currentUser = currentUserFixtures.adminUser;
+    const expectedHeaders = ["id", "Name", "Term"];
+    const expectedFields = ["id", "name", "term"];
+    const testId = "CoursesTable";
+    
+    // act
+    render(
+      <QueryClientProvider client={queryClient}>
+        <MemoryRouter>
+          <CoursesTable courses={[]} currentUser={currentUser} />
+        </MemoryRouter>
+      </QueryClientProvider>
+    );
+
+    // assert
+    expectedHeaders.forEach((headerText) => {
+      const header = screen.getByText(headerText);
+      expect(header).toBeInTheDocument();
+    });
+
+    expectedFields.forEach((field) => {
+      const fieldElement = screen.queryByTestId(`${testId}-cell-row-0-col-${field}`);
+      expect(fieldElement).not.toBeInTheDocument();
+    });
+
+    const totalCoursesElement = screen.getByText("Total Courses: 0"); // Since the table is empty
+    expect(totalCoursesElement).toBeInTheDocument();
+  });
+
+
+  test("Has the expected colum headers and content for adminUser", () => {
+
+    const currentUser = currentUserFixtures.adminUser;
+
+    render(
+      <QueryClientProvider client={queryClient}>
+        <MemoryRouter>
+            <CoursesTable courses={coursesFixtures.threeCourses} currentUser={currentUser} />
+        </MemoryRouter>
+      </QueryClientProvider>
+
+    );
+
+    const expectedHeaders = ["id", "Name", "Term"];
+    const expectedFields = ["id", "name", "term"];
+    const testId = "CoursesTable";
+
+    expectedHeaders.forEach((headerText) => {
+      const header = screen.getByText(headerText);
+      expect(header).toBeInTheDocument();
+    });
+
+    expectedFields.forEach((field) => {
+      const header = screen.getByTestId(`${testId}-cell-row-0-col-${field}`);
+      expect(header).toBeInTheDocument();
+    });
+
+    expect(screen.getByTestId(`${testId}-cell-row-0-col-id`)).toHaveTextContent("1");
+    expect(screen.getByTestId(`${testId}-cell-row-1-col-id`)).toHaveTextContent("2");
+
+    const joinButton = screen.queryByTestId(`${testId}-cell-row-0-col-Join-button`);
+    expect(joinButton).toBeInTheDocument(); 
+
+    const editButton = screen.getByTestId(`${testId}-cell-row-0-col-Edit-button`);
+    expect(editButton).toBeInTheDocument();
+    expect(editButton).toHaveClass("btn-primary");
+
+    const deleteButton = screen.getByTestId(`${testId}-cell-row-0-col-Delete-button`);
+    expect(deleteButton).toBeInTheDocument();
+    expect(deleteButton).toHaveClass("btn-danger");
+
+    const totalCoursesElement = screen.getByText("Total Courses: 3"); // Assuming there are 3 courses in the fixture
+    expect(totalCoursesElement).toBeInTheDocument();
+  });
+
+  test("Has the expected colum headers and content for instructorUser", () => {
+
+    const currentUser = currentUserFixtures.instructorUser;
+
+    render(
+      <QueryClientProvider client={queryClient}>
+        <MemoryRouter>
+            <CoursesTable courses={coursesFixtures.threeCourses} currentUser={currentUser} />
+        </MemoryRouter>
+      </QueryClientProvider>
+
+    );
+
+    const expectedHeaders = ["id", "Name", "Term"];
+    const expectedFields = ["id", "name", "term"];
+    const testId = "CoursesTable";
+
+    expectedHeaders.forEach((headerText) => {
+      const header = screen.getByText(headerText);
+      expect(header).toBeInTheDocument();
+    });
+
+    expectedFields.forEach((field) => {
+      const header = screen.getByTestId(`${testId}-cell-row-0-col-${field}`);
+      expect(header).toBeInTheDocument();
+    });
+
+    expect(screen.getByTestId(`${testId}-cell-row-0-col-id`)).toHaveTextContent("1");
+    expect(screen.getByTestId(`${testId}-cell-row-1-col-id`)).toHaveTextContent("2");
+
+    const joinButton = screen.queryByTestId(`${testId}-cell-row-0-col-Join-button`);
+    expect(joinButton).toBeInTheDocument(); 
+
+    // const staffButton = screen.getByTestId(`${testId}-cell-row-0-col-Staff-button`);
+    // expect(staffButton).toBeInTheDocument();
+    // expect(staffButton).toHaveClass("btn-primary");
+
+    const editButton = screen.getByTestId(`${testId}-cell-row-0-col-Edit-button`);
+    expect(editButton).toBeInTheDocument();
+    expect(editButton).toHaveClass("btn-primary");
+
+    const deleteButton = screen.getByTestId(`${testId}-cell-row-0-col-Delete-button`);
+    expect(deleteButton).toBeInTheDocument();
+    expect(deleteButton).toHaveClass("btn-danger");
+
+  });
+
+  test("Join button navigates to the join page for a user", async () => {
+
+    const currentUser = currentUserFixtures.userOnly;
+
+
+    render(
+      <QueryClientProvider client={queryClient}>
+        <MemoryRouter>
+            <CoursesTable courses={coursesFixtures.threeCourses} currentUser={currentUser} />
+        </MemoryRouter>
+      </QueryClientProvider>
+
+    );
+
+    await waitFor(() => { expect(screen.getByTestId(`CoursesTable-cell-row-0-col-id`)).toHaveTextContent("1"); });
+   const joinButton = screen.getByTestId(`CoursesTable-cell-row-0-col-Join-button`);
+    expect(joinButton).toBeInTheDocument();
+
+    fireEvent.click(joinButton);
+
+    await waitFor(() => expect(mockedNavigate).toHaveBeenCalledWith('/courses/join/1'));
+
+  });
+
+  test("Edit button navigates to the edit page for admin user", async () => {
+
+    const currentUser = currentUserFixtures.adminUser;
+
+    render(
+      <QueryClientProvider client={queryClient}>
+        <MemoryRouter>
+            <CoursesTable courses={coursesFixtures.threeCourses} currentUser={currentUser} />
+        </MemoryRouter>
+      </QueryClientProvider>
+
+    );
+
+    await waitFor(() => { expect(screen.getByTestId(`CoursesTable-cell-row-0-col-id`)).toHaveTextContent("1"); });
+
+    const editButton = screen.getByTestId(`CoursesTable-cell-row-0-col-Edit-button`);
+    expect(editButton).toBeInTheDocument();
+
+    fireEvent.click(editButton);
+
+    await waitFor(() => expect(mockedNavigate).toHaveBeenCalledWith('/courses/edit/1'));
+    
+    const totalCoursesElement = screen.getByText("Total Courses: 3"); // Assuming there are 3 courses in the fixture
+    expect(totalCoursesElement).toBeInTheDocument();
+    
+  });
+
+
+  test("Delete button calls the callback", async () => {
+
+    const currentUser = currentUserFixtures.adminUser;
+
+    render(
+      <QueryClientProvider client={queryClient}>
+        <MemoryRouter>
+            <CoursesTable courses={coursesFixtures.threeCourses} currentUser={currentUser} />
+        </MemoryRouter>
+      </QueryClientProvider>
+
+    );
+
+    await waitFor(() => { expect(screen.getByTestId(`CoursesTable-cell-row-0-col-id`)).toHaveTextContent("1"); });
+
+    const deleteButton = screen.getByTestId(`CoursesTable-cell-row-0-col-Delete-button`);
+    expect(deleteButton).toBeInTheDocument();
+
+    fireEvent.click(deleteButton);
+
+    const totalCoursesElement = screen.getByText("Total Courses: 3"); // Assuming there are 3 courses in the fixture
+    expect(totalCoursesElement).toBeInTheDocument();
+  });
+
+});
