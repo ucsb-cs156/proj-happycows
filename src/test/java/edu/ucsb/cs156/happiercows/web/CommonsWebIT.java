b843cca1fbc33e00ddb99c785a3632abf0f9f4fb
Justin Wu <justin_wu@ucsb.edu>
2024-11-25 23:38:31 -0800

    add web int test that edits/deletes a commons

--- src/test/java/edu/ucsb/cs156/happiercows/web/CommonsWebIT.java
+++ src/test/java/edu/ucsb/cs156/happiercows/web/CommonsWebIT.java
@@ -32,4 +32,37 @@ public class CommonsWebIT extends WebTestCase {
 
         assertThat(page.getByTestId("commonsCard-name-1")).hasText("Web Test Commons");
     }
+
+    @Test
+    public void adminCanEditAndDeleteCommonsTest() throws Exception {
+        setupUser(true);
+
+        page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Admin")).click();
+        page.getByText("Create Commons").click();
+
+        page.getByTestId("CommonsForm-name").fill("Web Test Commons");
+        page.getByTestId("CommonsForm-Submit-Button").click();
+
+        page.getByTestId("commonsCard-name-1").click();
+
+        page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Admin")).click();
+        page.getByText("List Commons").click();
+
+        assertThat(page.getByTestId("CommonsTable-cell-row-0-col-commons.name")).hasText("Web Test Commons");
+
+        page.getByTestId("CommonsTable-cell-row-0-col-Edit-button").click();
+        
+        page.getByTestId("CommonsForm-name").fill("WTC");
+        page.getByTestId("CommonsForm-Submit-Button").click();
+
+        assertThat(page.getByTestId("CommonsTable-cell-row-0-col-commons.name")).hasText("WTC");
+
+        page.getByTestId("CommonsTable-cell-row-0-col-Delete-button").click();
+        page.getByTestId("CommonsTable-Modal-Delete").click();
+        
+        // return to home page
+        page.getByText("Happy Cows").click(); 
+        
+        assertThat(page.getByText("There are currently no commons to join")).isVisible();
+    }
 }
