c9f99f36d87eb7ba75d4a87f28292129134afce4
yungong <yungong@ucsb.edu>
2024-11-26 04:15:59 +0000

    yungong - try to add CommonsPlay exception test

--- frontend/src/main/components/Commons/CommonsPlay.js
+++ frontend/src/main/components/Commons/CommonsPlay.js
@@ -3,11 +3,11 @@ import greetingsList from "../../../assets/PlayGreetings.json"
 import "../../pages/HomePage.css"
 import { Card } from "react-bootstrap";
 export default function CommonsPlay({ currentUser }) {
-  // Stryker disable  all 
+  
   const firstName = currentUser?.root ? currentUser?.root?.user?.givenName : "";
 
   const [welcomeText, _]= useState(greetingsList[Math.floor(Math.random() * greetingsList.length)]);
-  // Stryker restore all
+  
 
   return (
     <div data-testid="CommonsPlay">
