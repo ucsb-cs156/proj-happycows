cd603d6cc0005b85d934190bd5f5e7dde29c36bf
Aden <adenjo@ucsb.edu>
2024-11-26 21:16:52 -0800

    Added java.util.Optional line in studentrepository

--- src/main/java/edu/ucsb/cs156/happiercows/repositories/StudentRepository.java
+++ src/main/java/edu/ucsb/cs156/happiercows/repositories/StudentRepository.java
@@ -5,6 +5,8 @@ import edu.ucsb.cs156.happiercows.entities.Student;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface StudentRepository extends CrudRepository<Student, Integer> {
     Optional<Student> findById(Long id);
