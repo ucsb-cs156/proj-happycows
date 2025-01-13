6390959700a19cba03152de3f2a95a4c8c6af4e7
Phillip Conrad <pconrad@cs.ucsb.edu>
2025-01-13 14:06:03 -0800

    pc - add two new strategies for health update formulas
    Per emails from Mattanjah de Vries, week of Monday 01/06 through 01/13

--- src/main/java/edu/ucsb/cs156/happiercows/strategies/CowHealthUpdateStrategies.java
+++ src/main/java/edu/ucsb/cs156/happiercows/strategies/CowHealthUpdateStrategies.java
@@ -1,29 +1,36 @@
 package edu.ucsb.cs156.happiercows.strategies;
 
-
 import edu.ucsb.cs156.happiercows.entities.CommonsPlus;
 import edu.ucsb.cs156.happiercows.entities.UserCommons;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
 
 /**
- * The CowHealthUpdateStrategies enum provides a variety of strategies for updating cow health.
+ * The CowHealthUpdateStrategies enum provides a variety of strategies for
+ * updating cow health.
  *
- * For information on Java enum's, see the Oracle Java Tutorial on <a href="https://docs.oracle.com/javase/tutorial/java/javaOO/enum.html">Enum Types</a>,
+ * For information on Java enum's, see the Oracle Java Tutorial on
+ * <a href="https://docs.oracle.com/javase/tutorial/java/javaOO/enum.html">Enum
+ * Types</a>,
  * which are far more powerful in Java than enums in most other languages.
  */
 
 @Getter
 @AllArgsConstructor
+@Slf4j
 public enum CowHealthUpdateStrategies implements CowHealthUpdateStrategy {
 
-    Linear("Linear", "Cow health increases/decreases proportionally to the number of cows over/under the carrying capacity.") {
+    Linear("Linear",
+            "Cow health increases/decreases proportionally to the number of cows over/under the carrying capacity.") {
         @Override
         public double calculateNewCowHealth(CommonsPlus commonsPlus, UserCommons uC, int totalCows) {
-            return uC.getCowHealth() - (totalCows - commonsPlus.getEffectiveCapacity()) * commonsPlus.getCommons().getDegradationRate();
+            return uC.getCowHealth()
+                    - (totalCows - commonsPlus.getEffectiveCapacity()) * commonsPlus.getCommons().getDegradationRate();
         }
     },
-    Constant("Constant", "Cow health changes increases/decreases by the degradation rate, depending on if the number of cows exceeds the carrying capacity.") {
+    Constant("Constant",
+            "Cow health changes increases/decreases by the degradation rate, depending on if the number of cows exceeds the carrying capacity.") {
         @Override
         public double calculateNewCowHealth(CommonsPlus commonsPlus, UserCommons uC, int totalCows) {
             if (totalCows <= commonsPlus.getEffectiveCapacity()) {
@@ -37,6 +44,39 @@ public enum CowHealthUpdateStrategies implements CowHealthUpdateStrategy {
         @Override
         public double calculateNewCowHealth(CommonsPlus commonsPlus, UserCommons uC, int totalCows) {
             return uC.getCowHealth();
+        }
+    },
+    Milan("Milan",
+            "Cow health increases/decreases proportionally to the square of ratio of cows/total capacity according to a formula from Milan de Vries.") {
+        @Override
+        public double calculateNewCowHealth(CommonsPlus commonsPlus, UserCommons uC, int totalCows) {
+            double excess = totalCows - commonsPlus.getEffectiveCapacity();
+            double adjustmentFactor = 1.0;
+            double x = (excess / commonsPlus.getEffectiveCapacity());
+            if (excess > 0) {
+                adjustmentFactor = 1.0 - x * x;
+            } else {
+                adjustmentFactor = 1.0 + x * x;
+            }
+            adjustmentFactor = Math.max(0.0, adjustmentFactor);
+            return uC.getCowHealth() * adjustmentFactor;
+        }
+    },
+    Mattanjah("Mattanjah",
+            "Cow health increases/decreases proportionally to the square of ratio of excess/total capacity * degradation rate according to a formula from Mattanjah de Vries.") {
+        @Override
+        public double calculateNewCowHealth(CommonsPlus commonsPlus, UserCommons uC, int totalCows) {
+
+            double excess = totalCows - commonsPlus.getEffectiveCapacity();
+            double adjustmentFactor = 1.0;
+            double x = (excess / commonsPlus.getEffectiveCapacity()) * commonsPlus.getCommons().getDegradationRate();
+            if (excess > 0) {
+                adjustmentFactor = 1 - x * x;
+            } else {
+                adjustmentFactor = 1 + x * x;
+            }
+            adjustmentFactor = Math.max(0.0, adjustmentFactor);
+            return uC.getCowHealth() * adjustmentFactor;
         }
     };
 
