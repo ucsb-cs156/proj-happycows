1c1989f7b8edb9013b67cf85e281f29976c03616
Daniel Jensen <djensen2@outlook.com>
2025-08-22 14:37:25 -0700

    dj - vite migration

--- frontend/.storybook/main.js
+++ frontend/.storybook/main.js
@@ -1,16 +1,11 @@
-module.exports = {
-  core: {
-    builder: 'webpack5',
+/** @type { import('@storybook/react-vite').StorybookConfig } */
+const config = {
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  addons: ["@storybook/addon-links", "@storybook/addon-docs"],
+  framework: {
+    name: "@storybook/react-vite",
+    options: {},
   },
-  "stories": [
-    "../src/**/*.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/preset-create-react-app",
-    "@storybook/addon-docs"
-  ],
- framework: '@storybook/react-webpack5',
- staticDirs: ['../public'],
-}
+  staticDirs: ["../public"],
+};
+export default config;
