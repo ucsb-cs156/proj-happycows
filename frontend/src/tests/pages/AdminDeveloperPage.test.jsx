c499b3a2995fba376c1777409187165abf866820
Jackson Cao <jacksoncao@ucsb.edu>
2025-11-13 13:43:50 -0800

    added AdminDeveloperPage

---
+++ frontend/src/tests/pages/AdminDeveloperPage.test.jsx
@@ -0,0 +1,43 @@
+import { render, screen } from "@testing-library/react";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { MemoryRouter } from "react-router-dom";
+import axios from "axios";
+import AxiosMockAdapter from "axios-mock-adapter";
+
+import AdminDeveloperPage from "main/pages/AdminDeveloperPage";
+import { systemInfoFixtures } from "fixtures/systemInfoFixtures";
+import { apiCurrentUserFixtures } from "fixtures/currentUserFixtures";
+
+describe("AdminDeveloperPage tests", () => {
+  const queryClient = new QueryClient();
+  const axiosMock = new AxiosMockAdapter(axios);
+
+  beforeEach(() => {
+    axiosMock.reset();
+    axiosMock.resetHistory();
+    axiosMock
+      .onGet("/api/currentUser")
+      .reply(200, apiCurrentUserFixtures.userOnly);
+    axiosMock
+      .onGet("/api/systemInfo")
+      .reply(200, systemInfoFixtures.showingNeither);
+  });
+
+  test("renders without crashing", async () => {
+    render(
+      <QueryClientProvider client={queryClient}>
+        <MemoryRouter>
+          <AdminDeveloperPage />
+        </MemoryRouter>
+      </QueryClientProvider>,
+    );
+    expect(
+      await screen.findByText("Github Branch Information"),
+    ).toBeInTheDocument();
+    expect(
+      await screen.findByText(
+        "The following SystemInfo is displayed in a JSON file.",
+      ),
+    ).toBeInTheDocument();
+  });
+});
