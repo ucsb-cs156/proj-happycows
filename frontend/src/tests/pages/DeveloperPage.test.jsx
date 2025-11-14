da260d084f6f27c36eaaa159aa6b438173e6c830
Fernando Velazquez <fvelazquez@ucsb.edu>
2025-11-13 16:00:12 -0800

    fv - Added DeveloperPage Tests

---
+++ frontend/src/tests/pages/DeveloperPage.test.jsx
@@ -0,0 +1,50 @@
+import { render, screen } from "@testing-library/react";
+import DeveloperPage from "main/pages/DeveloperPage";
+import { useSystemInfo } from "main/utils/systemInfo";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { MemoryRouter } from "react-router";
+import { vi } from "vitest";
+
+vi.mock("main/utils/systemInfo", () => ({
+  useSystemInfo: vi.fn(),
+}));
+
+vi.mock("main/utils/currentUser", async () => {
+  return {
+    useCurrentUser: () => ({
+      data: {
+        loggedIn: true,
+        root: { user: { email: "test@ucsb.edu" } },
+      },
+    }),
+    useLogout: () => ({
+      mutate: vi.fn(),
+    }),
+    hasRole: () => true,
+  };
+});
+
+describe("DeveloperPage tests", () => {
+  const queryClient = new QueryClient();
+
+  test("renders Developer Info heading", () => {
+    useSystemInfo.mockReturnValue({
+      data: {
+        sourceRepo: "https://github.com/ucsb-cs156/proj-happycows",
+      },
+    });
+
+    render(
+      <QueryClientProvider client={queryClient}>
+        <MemoryRouter>
+          <DeveloperPage />
+        </MemoryRouter>
+      </QueryClientProvider>
+    );
+
+    expect(screen.getByText("Developer Info")).toBeInTheDocument();
+    expect(
+      screen.getByText("https://github.com/ucsb-cs156/proj-happycows")
+    ).toBeInTheDocument();
+  });
+});
