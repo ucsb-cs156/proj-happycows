5a985742b9cb2368b2b0eaa0aa9448d58a8b7830
Tim Nguyen <tvn@ucsb.edu>
2025-11-13 17:23:56 -0800

    Added RecordCommonStats job launcher to Admin Jobs page

--- frontend/src/main/pages/AdminJobsPage.jsx
+++ frontend/src/main/pages/AdminJobsPage.jsx
@@ -7,6 +7,7 @@ import UpdateCowHealthForm from "main/components/Jobs/UpdateCowHealthForm";
 import MilkCowsJobForm from "main/components/Jobs/MilkCowsJobForm";
 import InstructorReportForm from "main/components/Jobs/InstructorReportForm";
 import InstructorReportSpecificCommonsForm from "main/components/Jobs/InstructorReportSpecificCommonsForm";
+import RecordCommonStatsForm from "main/components/Jobs/RecordCommonStatsForm";
 import { toast } from "react-toastify";
 
 import { useBackendMutation } from "main/utils/useBackend";
@@ -170,6 +171,26 @@ const AdminJobsPage = () => {
     InstructorReportSpecificCommonsMutation.mutate(data);
   };
 
+  // *** Record Common Stats job ***
+
+  const objectToAxiosParamsRecordCommonStatsJob = () => ({
+    url: `/api/jobs/launch/recordcommonstats`,
+    method: "POST",
+  });
+
+  // Stryker disable all
+  const RecordCommonStatsMutation = useBackendMutation(
+    objectToAxiosParamsRecordCommonStatsJob,
+    {},
+    ["/api/jobs/all"],
+  );
+  // Stryker restore all
+
+  const submitRecordCommonStatsJob = async () => {
+    toast("Submitted Job: Record Common Stats");
+    RecordCommonStatsMutation.mutate();
+  };
+
   const jobLaunchers = [
     {
       name: "Test Job",
@@ -198,6 +219,10 @@ const AdminJobsPage = () => {
           submitAction={submitInstructorReportSpecificCommonsJob}
         />
       ),
+    },
+    {
+      name: "Record Common Stats",
+      form: <RecordCommonStatsForm submitAction={submitRecordCommonStatsJob} />,
     },
   ];
 
