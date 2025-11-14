043dd025c6b0123b1d8d8cbe8917b8fed8cedf27
Catherine Zhong <catherinezhong@ucsb.edu>
2025-11-13 17:05:55 -0800

    changed title to Happycows and added correct favicon

---
+++ frontend/src/tests/index_html.test.jsx
@@ -0,0 +1,23 @@
+import fs from "fs";
+import path from "path";
+
+beforeAll(() => {
+    // Load the real index.html into JSDOM
+    const filePath = path.resolve("index.html");
+    const html = fs.readFileSync(filePath, "utf-8");
+    document.documentElement.innerHTML = html;
+  });
+
+//checks for title
+describe("index.html metadata", () => {
+  it("sets the correct document title", () => {
+    expect(document.title).toBe("HappyCows");
+  });
+
+//checks of favicon
+  it("has the correct favicon", () => {
+    const icon = document.querySelector("link[rel~='icon']");
+    expect(icon).not.toBeNull();
+    expect(icon.getAttribute("href")).toBe("/public/favicon.ico");
+  });
+});
