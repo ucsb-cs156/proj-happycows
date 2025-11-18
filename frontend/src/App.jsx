9e934ada7b21cdafe46b3bf98dca453b865cffef
William Lee <wplee@ucsb.edu>
2025-11-18 14:35:50 -0800

    added admin/developer info page and tests

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -29,6 +29,7 @@ import NotFoundPage from "main/pages/NotFoundPage";
 import AdminViewPlayPage from "main/pages/AdminViewPlayPage";
 import AdminAnnouncementsPage from "main/pages/AdminAnnouncementsPage";
 import AdminCreateAnnouncementsPage from "main/pages/AdminCreateAnnouncementsPage";
+import DeveloperPage from "main/pages/DeveloperPage";
 
 function App() {
   const { data: currentUser } = useCurrentUser();
@@ -66,6 +67,7 @@ function App() {
         path="/admin/announcements/:commonsId/create"
         element={<AdminCreateAnnouncementsPage />}
       />
+      <Route path="/developer" element={<DeveloperPage />} />
     </>
   ) : null;
 
