0cc4fc54dd2433e3e7e922e7bd71b8f0f51ef49d
Nataly Gonzalez Ornelas <ngonzalezornelas@ucsb.edu>
2025-11-22 12:48:47 -0800

    n-ornelas - Created GET for single course and added tests

--- src/main/java/edu/ucsb/cs156/happiercows/controllers/CourseController.java
+++ src/main/java/edu/ucsb/cs156/happiercows/controllers/CourseController.java
@@ -1,9 +1,12 @@
 package edu.ucsb.cs156.happiercows.controllers;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import edu.ucsb.cs156.happiercows.entities.Course;
+import edu.ucsb.cs156.happiercows.errors.EntityNotFoundException;
 import edu.ucsb.cs156.happiercows.repositories.CourseRepository;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -19,7 +22,7 @@ public class CourseController extends ApiController {
 
     /**
      * This method returns a list of all courses.
-     * 
+     *
      * @return a list of all courses
      */
     @Operation(summary = "List all courses")
@@ -28,5 +31,18 @@ public class CourseController extends ApiController {
     public Iterable<Course> allOrganisations() {
         Iterable<Course> courses = courseRepository.findAll();
         return courses;
+    }
+
+    @Operation(summary = "Get a single course")
+    @PreAuthorize("hasRole('ROLE_USER')")
+    @GetMapping("")
+
+    public Course getById(@Parameter(name = "id") @RequestParam Long id) {
+        Course course =
+            courseRepository
+                .findById(id)
+                .orElseThrow(() -> new EntityNotFoundException(Course.class, id));
+
+        return course;
     }
 }
