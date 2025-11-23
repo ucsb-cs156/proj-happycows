4444485a0d7839e4ed72f302476a8be0449ba94b
Mihir Srivastava <m_srivastava@ucsb.edu>
2025-11-23 15:18:38 -0800

    added GET functionality and tests with all passing

--- src/main/java/edu/ucsb/cs156/happiercows/controllers/CourseController.java
+++ src/main/java/edu/ucsb/cs156/happiercows/controllers/CourseController.java
@@ -1,9 +1,11 @@
 package edu.ucsb.cs156.happiercows.controllers;
 
 import edu.ucsb.cs156.happiercows.entities.Course;
+import edu.ucsb.cs156.happiercows.errors.EntityNotFoundException;
 import edu.ucsb.cs156.happiercows.repositories.CourseRepository;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -28,5 +30,15 @@ public class CourseController extends ApiController {
     public Iterable<Course> allOrganisations() {
         Iterable<Course> courses = courseRepository.findAll();
         return courses;
+    }
+
+    @Operation(summary = "Get a course by id")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @GetMapping("/{id}")
+    public Course getCourseById(
+            @Parameter(name = "id") @PathVariable Long id) {
+        Course course = courseRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException(Course.class, id));
+        return course;
     }
 }
