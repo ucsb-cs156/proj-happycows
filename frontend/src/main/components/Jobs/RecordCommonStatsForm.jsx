ee7926372404e194c98066716989db1dee5167a4
Ivy Holiday <iholiday@ucsb.edu>
2026-05-14 13:29:21 -0700

    Implemented RecordCommonStatsForm, copying the style of SetCowHealthForm

--- frontend/src/main/components/Jobs/RecordCommonStatsForm.jsx
+++ frontend/src/main/components/Jobs/RecordCommonStatsForm.jsx
@@ -1,10 +1,18 @@
 import { Button, Form } from "react-bootstrap";
 import { useForm } from "react-hook-form";
 
-function RecordCommonStatsForm({ submitAction }) {
-  const { handleSubmit } = useForm();
+function RecordCommonStatsForm({ 
+  submitAction = () => {},
+  testid = "RecordCommonStatsForm",
+ }) {
+  const { handleSubmit } = useForm(); //wraps form submission to handle the form data 
+  const onSubmit = (data) => { //create handler function
+    submitAction(data)
+  };
   return (
-
+    <Form onSubmit={handleSubmit(onSubmit)} data-testid={testid}>
+      <Button type="submit" data-testid="RecordCommonStatsForm-Submit-Button">Record Stats</Button>
+    </Form>
   )
 }
 
