d77ea6eb457bc6ffb6d7ea7e7bc89d3f9e3bbffc
Ivy Holiday <iholiday@ucsb.edu>
2026-05-14 12:52:06 -0700

    Created file for RecordCommonStatsForm.jsc

---
+++ frontend/src/main/components/Jobs/RecordCommonStatsForm.jsx
@@ -0,0 +1,11 @@
+import { Button, Form } from "react-bootstrap";
+import { useForm } from "react-hook-form";
+
+function RecordCommonStatsForm({ submitAction }) {
+  const { handleSubmit } = useForm();
+  return (
+
+  )
+}
+
+export default RecordCommonStatsForm;
