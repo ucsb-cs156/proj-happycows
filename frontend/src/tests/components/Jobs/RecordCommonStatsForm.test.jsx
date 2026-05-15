3522c9ae419f5bd0ccf795f2985e93ef3d6f611c
Ivy Holiday <iholiday@ucsb.edu>
2026-05-14 17:47:21 -0700

    Rewrote and cleaned up tests

--- frontend/src/tests/components/Jobs/RecordCommonStatsForm.test.jsx
+++ frontend/src/tests/components/Jobs/RecordCommonStatsForm.test.jsx
@@ -2,10 +2,6 @@ import { fireEvent, render, screen, waitFor } from "@testing-library/react";
 import { MemoryRouter as Router } from "react-router";
 import RecordCommonStatsForm from "main/components/Jobs/RecordCommonStatsForm";
 import { QueryClient, QueryClientProvider } from "react-query";
-import AxiosMockAdapter from "axios-mock-adapter";
-import axios from "axios";
-import commonsFixtures from "fixtures/commonsFixtures";
-import * as useBackendModule from "main/utils/useBackend";
 import { vi } from "vitest";
 
 // Next line uses technique from https://www.chakshunyu.com/blog/how-to-spy-on-a-named-import-in-jest/
@@ -18,12 +14,9 @@ vi.mock("react-router", async () => ({
 }));
 
 describe("RecordCommonStatsForm tests", () => {
-  const axiosMock = new AxiosMockAdapter(axios);
 
   //Test 1: Component renders without crashing
   it("renders the fallback text correctlyl", async () => {
-    axiosMock.onGet("/api/commons/all").reply(200, []);
-
     render(
       <QueryClientProvider client={new QueryClient()}>
         <Router>
@@ -31,18 +24,12 @@ describe("RecordCommonStatsForm tests", () => {
         </Router>
       </QueryClientProvider>,
     );
-
+    expect(screen.getByText(/Record statistics for all commons/i)).toBeInTheDocument();
+    expect(screen.getByText("RecordCommonStatsForm-Submit-Button")).toBeInTheDocument();
   });
   //Test 2: Submit button is present and clickable
   it("user can sucessfully submit the job", async () => {
-    const getItemSpy = vi.spyOn(Storage.prototype, "getItem");
-    getItemSpy.mockImplementation(() => null);
-
     const submitAction = vi.fn();
-    axiosMock
-      .onGet("/api/commons/all")
-      .reply(200, commonsFixtures.threeCommons);
-
     render(
       <QueryClientProvider client={new QueryClient()}>
         <Router>
@@ -50,28 +37,10 @@ describe("RecordCommonStatsForm tests", () => {
         </Router>
       </QueryClientProvider>,
     );
-
-  });
-
-  //Test 4: Form data is passed to submitAction
-  test("the correct parameters are passed to useBackend", async () => {
-    // https://www.chakshunyu.com/blog/how-to-spy-on-a-named-import-in-jest/
-    const useBackendSpy = vi.spyOn(useBackendModule, "useBackend");
-
-    render(
-      <QueryClientProvider client={new QueryClient()}>
-        <Router>
-          <RecordCommonStatsForm />
-        </Router>
-      </QueryClientProvider>,
-    );
-
+    const submitButton = screen.getByTestId("RecordCommonStatsForm-Submit-Button");
+    fireEvent.click(submitButton);
     await waitFor(() => {
-      expect(useBackendSpy).toHaveBeenCalledWith(
-        ["/api/commons/all"],
-        { url: "/api/commons/all" },
-        [],
-      );
-    });
+      expect(submitAction).toHaveBeenCalled();
+    })
   });
 });
