a544e8e9c9e67df52cb3be8434498e7ccf97f1fa
Nikita Huynh <nikitahuynh@ucsb.edu>
2026-05-14 17:58:11 -0700

    allow admin to delete messages from commons

--- frontend/src/main/pages/AdminChatPage.jsx
+++ frontend/src/main/pages/AdminChatPage.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ChatHistoryPage from "main/pages/ChatHistoryPage"; // adjust path if needed
 
 const AdminChatPage = () => {
-  return <ChatHistoryPage readOnly={true} />;
+  return <ChatHistoryPage readOnly={true} isAdmin={true} />;
 };
 
 export default AdminChatPage;
