236869bad2a7531b418d2e2ade6d033ca3929f7b
Rohan Nihalani <rohan.ved.nihalani@gmail.com>
2026-05-19 17:53:25 -0700

    added chat features to admin page and created new admin chat page

---
+++ frontend/src/tests/pages/AdminChatPage.test.jsx
@@ -0,0 +1,73 @@
+import { render, screen } from "@testing-library/react";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { MemoryRouter } from "react-router";
+import axios from "axios";
+import AxiosMockAdapter from "axios-mock-adapter";
+import { apiCurrentUserFixtures } from "fixtures/currentUserFixtures";
+import { systemInfoFixtures } from "fixtures/systemInfoFixtures";
+import AdminChatPage from "main/pages/AdminChatPage";
+import AdminListCommonsPage from "main/pages/AdminListCommonPage";
+import commonsPlusFixtures from "fixtures/commonsPlusFixtures";
+import { vi } from "vitest";
+
+const mockedNavigate = vi.fn();
+
+vi.mock("react-router", async () => ({
+  ...(await vi.importActual("react-router")),
+  useParams: () => ({
+    commonsId: 1,
+  }),
+  useNavigate: () => mockedNavigate,
+}));
+
+describe("AdminChatPage tests", () => {
+  const axiosMock = new AxiosMockAdapter(axios);
+  const queryClient = new QueryClient();
+
+  beforeEach(() => {
+    axiosMock.reset();
+    axiosMock.resetHistory();
+    axiosMock
+      .onGet("/api/currentUser")
+      .reply(200, apiCurrentUserFixtures.adminUser);
+    axiosMock
+      .onGet("/api/systemInfo")
+      .reply(200, systemInfoFixtures.showingNeither);
+  });
+
+  test("renders page without crashing", async () => {
+    render(
+      <QueryClientProvider client={queryClient}>
+        <MemoryRouter>
+          <AdminChatPage />
+        </MemoryRouter>
+      </QueryClientProvider>,
+    );
+  });
+
+  test("renders chat with correct commons name", async () => {
+    axiosMock
+      .onGet("/api/currentUser")
+      .reply(200, apiCurrentUserFixtures.adminUser);
+    axiosMock.onGet("/api/commons/plus", { params: { id: 1 } }).reply(200, {
+      commons: {
+        id: 1,
+        name: "Sample Commons",
+      },
+      totalPlayers: 5,
+      totalCows: 5,
+    });
+
+    render(
+      <QueryClientProvider client={queryClient}>
+        <MemoryRouter>
+          <AdminChatPage />
+        </MemoryRouter>
+      </QueryClientProvider>,
+    );
+
+    expect(
+      await screen.findByText("Chat for Commons: Sample Commons"),
+    ).toBeInTheDocument();
+  });
+});
