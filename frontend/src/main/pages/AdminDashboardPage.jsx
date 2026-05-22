4fb6a7540e9f5059cf6dd6676fa673ed90c7e96d
Joaquin Wong <joaquinwong@ucsb.edu>
2026-05-21 18:11:07 -0700

    jw added placeholder page for dashboard with a title

---
+++ frontend/src/main/pages/AdminDashboardPage.jsx
@@ -0,0 +1,11 @@
+import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
+
+export default function AdminDashboardPage() {
+  return (
+    <BasicLayout>
+      <div className="pt-2">
+        <h1>Admin Dashboard!</h1>
+      </div>
+    </BasicLayout>
+  );
+}
