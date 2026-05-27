68434a881877b871f78ea68bd3deff630b93c5db
Joy Cheng <joycheng@ucsb.edu>
2026-05-26 21:00:37 -0700

    Create, edit, and index pages

--- frontend/src/main/components/Courses/CoursesTable.jsx
+++ frontend/src/main/components/Courses/CoursesTable.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import OurTable, { ButtonColumn } from "main/components/OurTable";
-import { useNavigate } from "react-router";
+
 import { useBackendMutation } from "main/utils/useBackend";
-import { hasRole } from "main/utils/currentUser";
 import {
   cellToAxiosParamsDelete,
   onDeleteSuccess,
 } from "main/utils/courseUtils";
+import { useNavigate } from "react-router";
+import { hasRole } from "main/utils/currentUser";
 
 export default function CoursesTable({
   courses,
@@ -16,16 +17,19 @@ export default function CoursesTable({
   const navigate = useNavigate();
 
   const editCallback = (cell) => {
-    navigate(`/admin/editcourses/${cell.row.values.id}`);
+    navigate(`/admin/courses/edit/${cell.row.values.id}`);
   };
 
+  // Stryker disable all : hard to test query caching
   const deleteMutation = useBackendMutation(
     cellToAxiosParamsDelete,
     { onSuccess: onDeleteSuccess },
-    ["/api/course/all"],
+    [`/api/course/all`],
   );
+  // Stryker restore all
 
-  const deleteCallback = async (cell) => {
+  // Stryker disable next-line all : tested through useBackendMutation behavior
+  const deleteCallback = (cell) => {
     deleteMutation.mutate(cell);
   };
 
@@ -49,11 +53,19 @@ export default function CoursesTable({
   ];
 
   if (hasRole(currentUser, "ROLE_ADMIN")) {
-    columns.push(ButtonColumn("Edit", "primary", editCallback, testIdPrefix));
+    columns.push(
+      ButtonColumn("Edit", "primary", editCallback, testIdPrefix),
+    );
     columns.push(
       ButtonColumn("Delete", "danger", deleteCallback, testIdPrefix),
     );
   }
 
-  return <OurTable data={courses} columns={columns} testid={testIdPrefix} />;
+  return (
+    <OurTable
+      data={courses}
+      columns={columns}
+      testid={testIdPrefix}
+    />
+  );
 }
