f9703a98262cd05d041f884bb1fe9758b19dcec8
Phill Conrad <pconrad@cs.ucsb.edu>
2026-06-21 16:39:57 -0700

    pc - add msw to storybook for AdminDashboardPage

--- frontend/src/stories/pages/AdminDashboardPage.stories.jsx
+++ frontend/src/stories/pages/AdminDashboardPage.stories.jsx
@@ -34,3 +34,42 @@ Default.parameters = {
     }),
   ],
 };
+
+export const WithData = Template.bind({});
+
+WithData.parameters = {
+  msw: [
+    http.get("/api/currentUser", () => {
+      return HttpResponse.json(apiCurrentUserFixtures.adminUser, {
+        status: 200,
+      });
+    }),
+    http.get("/api/systemInfo", () => {
+      return HttpResponse.json(systemInfoFixtures.showingNeither, {
+        status: 200,
+      });
+    }),
+    http.get("/api/commons/plus", () => {
+      return HttpResponse.json(
+        {
+          commons: {
+            id: 1,
+            name: "Sample Commons",
+          },
+          totalPlayers: 7,
+          totalCows: 55,
+          minCows: 1,
+          maxCows: 20,
+          avergeCows: 7.86,
+          stdDevCows: 6.23,
+        },
+        { status: 200 },
+      );
+    }),
+    http.get("/api/commons/numcows", () => {
+      return HttpResponse.json([1, 2, 3, 5, 10, 15, 20], {
+        status: 200,
+      });
+    }),
+  ],
+};
